9cc4857a60f970927d146d197ec5d02392613d07
mikezks <michael.zikes@intauria.com>
2020-01-22 09:43:43 +0100

    RxJS

--- projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { timer, Observable, Subscription, Subject, of, zip } from 'rxjs';
-import { tap, takeUntil, switchMap, map, debounceTime, filter } from 'rxjs/operators';
+import { timer, Observable, Subscription, Subject, of, zip, interval, combineLatest, iif } from 'rxjs';
+import { tap, takeUntil, switchMap, map, debounceTime, filter, distinctUntilChanged, startWith, share } from 'rxjs/operators';
 import { FormControl } from '@angular/forms';
 import { Flight } from '@flight-workspace/flight-api';
 import { HttpParams, HttpHeaders, HttpClient } from '@angular/common/http';
@@ -18,20 +18,45 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   control = new FormControl();
   flights$: Observable<Flight[]>;
   loading: boolean;
+  online$: Observable<boolean>;
+  online: boolean;
   
   constructor(private http: HttpClient) { }
   
   ngOnInit() {
     //this.rxjsDemo();
 
+    this.online$ = interval(2000)
+      .pipe(
+        startWith(0),
+        map(() => Math.random() < 0.5),
+        distinctUntilChanged(),
+        //tap(online => this.online = online),
+        tap(online => console.log('online', online)),
+        share()
+      );
+
     this.flights$ =
       this.control.valueChanges
         .pipe(
-          filter(from => from.length > 2),
+          from$ => combineLatest(from$, this.online$),
+          filter(([from, online]) => online),
+          map(([from, online]) => from),
+          distinctUntilChanged(),
+          //filter(from => from.length > 2),
           debounceTime(300),
-          tap(() => this.loading = true),
-          switchMap(from => this.load(from)),
-          tap(() => this.loading = false),
+          switchMap(from =>
+            iif(
+              () => from.length > 2,
+              of(from)
+                .pipe(
+                  tap(() => this.loading = true),
+                  switchMap(from => this.load(from)),
+                  tap(() => this.loading = false)
+                ),
+              of([])
+            )
+          )          
         );
   }
 
